1b7a81dff805a67c9a42f73522fd368ed97786c9
noufanelachola <noufansadiqelachola@gmail.com>
2023-09-09 15:06:55 +0530

    localstorage completed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect } from "react"
 import Sidebar from "./components/Sidebar"
 import Editor from "./components/Editor"
 import { data } from "./data"
@@ -15,11 +15,18 @@ import "react-mde/lib/styles/css/react-mde-all.css"
  */
 
 export default function App() {
-    const [notes, setNotes] = React.useState([])
+
+    
+
+    const [notes, setNotes] = React.useState(JSON.parse(localStorage.getItem("notes")) || [])
     const [currentNoteId, setCurrentNoteId] = React.useState(
         (notes[0] && notes[0].id) || ""
     )
-    console.log(currentNoteId)
+    
+    
+    useEffect(() => {
+        localStorage.setItem("notes",JSON.stringify(notes))
+    } , [notes])
     
     function createNewNote() {
         const newNote = {
